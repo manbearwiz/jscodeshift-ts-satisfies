7740b648f09648f9033b4837d95e5c4d8054b340
Kevin Brey <manbearwiz@gmail.com>
2023-08-24 17:00:29 -0500

    refactor: clean up tests

--- src/ts-satisfies.spec.ts
+++ src/ts-satisfies.spec.ts
@@ -1,116 +1,92 @@
-import { defineInlineTest } from 'jscodeshift/src/testUtils';
+import { runInlineTest } from 'jscodeshift/src/testUtils';
 import * as transform from './ts-satisfies';
+import { Options } from 'jscodeshift';
+
+function transformTest(
+  input: string,
+  expectedOutput: string,
+  options: Options = {},
+) {
+  runInlineTest(transform, options, { source: input }, expectedOutput);
+}
 
 describe('ts-satisfies', () => {
-  defineInlineTest(
-    transform,
-    {},
-    `const route: Route = { label: 'Label' };`,
-    `const route = { label: 'Label' } satisfies Route;`,
-    'handles simple types',
-  );
+  it('handles simple types', () => {
+    transformTest(
+      `const route: Route = { label: 'Label' };`,
+      `const route = { label: 'Label' } satisfies Route;`,
+    );
+  });
 
-  defineInlineTest(
-    transform,
-    { types: ['Bar', 'Biz'] },
-    `const A = {} as Foo, B: Bar = {}, C = {} as Biz;`,
-    `const A = {} as Foo, B = {} satisfies Bar, C = {} satisfies Biz;`,
-    'handles type restrictions',
-  );
+  it('handles type restrictions', () => {
+    transformTest(
+      `const A = {} as T, B: U = {}, C = {} as V;`,
+      `const A = {} as T, B = {} satisfies U, C = {} satisfies V;`,
+      { types: ['U', 'V'] },
+    );
+  });
 
-  defineInlineTest(
-    transform,
-    {},
-    `const a: T[] = [ { label: 'Label' } ];`,
-    `const a = [ { label: 'Label' } ] satisfies T[];`,
-    'handles array types',
-  );
+  it('handles array types', () => {
+    transformTest(
+      `const a: T[] = [ { a: 1 } ];`,
+      `const a = [ { a: 1 } ] satisfies T[];`,
+    );
+  });
 
-  defineInlineTest(
-    transform,
-    {},
-    `const a: T[] = []`,
-    `const a: T[] = []`,
-    'not modify empty arrays',
-  );
+  it('not modify empty arrays', () => {
+    transformTest(`const a: T[] = []`, `const a: T[] = []`);
+  });
 
-  defineInlineTest(
-    transform,
-    {},
-    `const meta: Meta<Foo> = { title: 'Title' }; `,
-    `const meta = { title: 'Title' } satisfies Meta<Foo>; `,
-    'handles generic types',
-  );
+  it('handles generic types', () => {
+    transformTest(
+      `const meta: Meta<T> = { title: 'Title' }; `,
+      `const meta = { title: 'Title' } satisfies Meta<T>; `,
+    );
+  });
 
-  defineInlineTest(
-    transform,
-    { types: ['Meta'] },
-    `const meta: Meta<Foo> = {}, bar: Bar = {}; `,
-    `const meta = {} satisfies Meta<Foo>, bar: Bar = {}; `,
-    'handles generic type restriction',
-  );
+  it('handles generic type restriction', () => {
+    transformTest(
+      `const meta: Meta<T> = {}, bar: Bar = {}; `,
+      `const meta = {} satisfies Meta<T>, bar: Bar = {}; `,
+      { types: ['Meta'] },
+    );
+  });
 
-  defineInlineTest(
-    transform,
-    {},
-    `const A = {} as Foo, B: Bar = {};`,
-    `const A = {} satisfies Foo, B = {} satisfies Bar;`,
-    'handles multiple declerations',
-  );
+  it('handles multiple declarations', () => {
+    transformTest(
+      `const A = {} as T, B: Bar = {};`,
+      `const A = {} satisfies T, B = {} satisfies Bar;`,
+    );
+  });
 
-  defineInlineTest(
-    transform,
-    {},
-    `const A: Foo = {} as Foo;`,
-    `const A = {} satisfies Foo;`,
-    'handles both type annotations',
-  );
+  it('handles both type annotations', () => {
+    transformTest(`const A: T = {} as T;`, `const A = {} satisfies T;`);
+  });
 
-  defineInlineTest(
-    transform,
-    {},
-    `f({ foo: null } as Foo);`,
-    `f({ foo: null } satisfies Foo);`,
-    'handles function args',
-  );
+  it('handles function args', () => {
+    transformTest(`f({ foo: null } as T);`, `f({ foo: null } satisfies T);`);
+  });
 
-  defineInlineTest(
-    transform,
-    {},
-    `const x: Fn = () => { return null; }`,
-    `const x: Fn = () => { return null; }`,
-    'not modify function',
-  );
+  it('not modify function', () => {
+    transformTest(`const x: Fn = () => null`, `const x: Fn = () => null`);
+  });
 
-  defineInlineTest(
-    transform,
-    {},
-    `const x = bar.foo as Foo`,
-    `const x = bar.foo as Foo`,
-    'not modify casts',
-  );
+  it('not modify casts', () => {
+    transformTest(`const x = bar.foo as T`, `const x = bar.foo as T`);
+  });
 
-  defineInlineTest(
-    transform,
-    {},
-    `const align = ['left', 'right'] as const`,
-    `const align = ['left', 'right'] as const`,
-    'not modify const assertions',
-  );
+  it('not modify const assertions', () => {
+    transformTest(
+      `const align = ['left', 'right'] as const`,
+      `const align = ['left', 'right'] as const`,
+    );
+  });
 
-  defineInlineTest(
-    transform,
-    {},
-    `fn({} as any as T)`,
-    `fn({} as any as T)`,
-    'not modify intentional any conversions in functions',
-  );
+  it('not modify intentional any conversions in functions', () => {
+    transformTest(`fn({} as any as T)`, `fn({} as any as T)`);
+  });
 
-  defineInlineTest(
-    transform,
-    {},
-    `const a = ({} as any) as T`,
-    `const a = ({} as any) as T`,
-    'not modify intentional any conversions in declerations',
-  );
+  it('not modify intentional any conversions in declarations', () => {
+    transformTest(`const a = ({} as any) as T`, `const a = ({} as any) as T`);
+  });
 });
