6a452e4ca40419c5be6d1c899c077f98e3e6f16c
Kevin Brey <manbearwiz@gmail.com>
2023-08-25 14:16:11 -0500

    refactor: use type guards from ast-types

--- src/ts-satisfies.ts
+++ src/ts-satisfies.ts
@@ -1,48 +1,22 @@
-import {
+import type {
   API,
   ArrayExpression,
   FileInfo,
   Identifier,
+  JSCodeshift,
   Node,
   ObjectExpression,
   Options,
-  TSArrayType,
-  TSAsExpression,
-  TSTypeReference,
   VariableDeclarator,
 } from 'jscodeshift';
 
-interface NodeTypeMap {
-  ArrayExpression: ArrayExpression;
-  ArrayOrObjectExpression: ArrayExpression | ObjectExpression;
-  Identifier: Identifier;
-  ObjectExpression: ObjectExpression;
-  TSArrayType: TSArrayType;
-  TSAsExpression: TSAsExpression;
-  TSTypeReference: TSTypeReference;
-  VariableDeclarator: VariableDeclarator;
-}
-
-function nodeTypeGuard<T extends keyof NodeTypeMap>(
-  type: T,
-): (node?: Node | null) => node is NodeTypeMap[T] {
-  return (node?: Node | null): node is NodeTypeMap[T] => node?.type === type;
-}
-
-const isArrayExpression = nodeTypeGuard('ArrayExpression');
-const isIdentifier = nodeTypeGuard('Identifier');
-const isObjectExpression = nodeTypeGuard('ObjectExpression');
-const isTSArrayType = nodeTypeGuard('TSArrayType');
-const isTSAsExpression = nodeTypeGuard('TSAsExpression');
-const isTSTypeReference = nodeTypeGuard('TSTypeReference');
-const isVariableDeclarator = nodeTypeGuard('VariableDeclarator');
-
 function isArrayOrObjectExpression(
+  j: JSCodeshift,
   node?: Node | null,
 ): node is ArrayExpression | ObjectExpression {
   return (
-    (isArrayExpression(node) && !!node.elements.length) ||
-    isObjectExpression(node)
+    (j.ArrayExpression.check(node) && !!node.elements.length) ||
+    j.ObjectExpression.check(node)
   );
 }
 
@@ -59,33 +33,32 @@ export default function transform(
 
   root.find(j.VariableDeclaration).map((path) => {
     path.node.declarations
-      .filter(isVariableDeclarator)
+      .filter((x): x is VariableDeclarator => j.VariableDeclarator.check(x))
       .filter(
         (d) =>
-          d.id.type === 'Identifier' &&
-          ((isTSAsExpression(d?.init) &&
-            isArrayOrObjectExpression(d.init.expression)) ||
-            (d.id.typeAnnotation && isArrayOrObjectExpression(d.init))),
+          j.Identifier.check(d.id) &&
+          ((j.TSAsExpression.check(d?.init) &&
+            isArrayOrObjectExpression(j, d.init.expression)) ||
+            (d.id.typeAnnotation && isArrayOrObjectExpression(j, d.init))),
       )
       .forEach((declarator) => {
         const id = declarator.id as Identifier;
-        const annotation = isTSAsExpression(declarator.init)
+        const annotation = j.TSAsExpression.check(declarator.init)
           ? declarator.init.typeAnnotation
           : id.typeAnnotation?.typeAnnotation;
-        const init =
-          isTSAsExpression(declarator.init) && !!declarator.init
-            ? declarator.init.expression
-            : declarator.init;
+        const init = j.TSAsExpression.check(declarator.init)
+          ? declarator.init.expression
+          : declarator.init;
 
         if (
           init &&
-          (isTSArrayType(annotation) ||
-            (isTSTypeReference(annotation) &&
-              isIdentifier(annotation.typeName) &&
+          (j.TSArrayType.check(annotation) ||
+            (j.TSTypeReference.check(annotation) &&
+              j.Identifier.check(annotation.typeName) &&
               annotation.typeName.name !== 'const')) &&
           (!typeRestriction ||
-            (isTSTypeReference(annotation) &&
-              isIdentifier(annotation.typeName) &&
+            (j.TSTypeReference.check(annotation) &&
+              j.Identifier.check(annotation.typeName) &&
               typeRestriction.includes(annotation.typeName.name)))
         ) {
           declarator.init = j.tsSatisfiesExpression(init, annotation);
@@ -97,7 +70,8 @@ export default function transform(
 
   root.find(j.CallExpression).forEach(({ node }) => {
     node.arguments = node.arguments.map((arg) =>
-      isTSAsExpression(arg) && isArrayOrObjectExpression(arg.expression)
+      j.TSAsExpression.check(arg) &&
+      isArrayOrObjectExpression(j, arg.expression)
         ? j.tsSatisfiesExpression(arg.expression, arg.typeAnnotation)
         : arg,
     );
