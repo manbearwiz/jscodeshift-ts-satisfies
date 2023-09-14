0dab32fff7d5aa2283372590526c0759cb3f9611
Kevin Brey <manbearwiz@gmail.com>
2023-09-14 09:11:29 -0500

    test: lower code coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,10 +5,10 @@ export default defineConfig({
     environment: 'happy-dom',
     globals: true,
     coverage: {
-      statements: 100,
-      branches: 100,
-      functions: 100,
-      lines: 100,
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
     },
   },
 });
